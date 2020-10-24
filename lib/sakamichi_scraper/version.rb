5839fe2c6d28b8c6d8fd960bc93de1a2b9ad1a90
oyrarumakan <ryo19911030@hotmail.co.jp>
2020-10-24 22:56:11 +0900

    :new: Initial commit

---
+++ lib/sakamichi_scraper/version.rb
@@ -0,0 +1,3 @@
+module SakamichiScraper
+  VERSION = "0.1.0"
+end
---
+++ spec/sakamichi_scraper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SakamichiScraper do
+  it "has a version number" do
+    expect(SakamichiScraper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
