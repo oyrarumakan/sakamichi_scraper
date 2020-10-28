0f5d01edfa1d3ba224fd9576e003ba514d567dab
oyrarumakan <ryo19911030@hotmail.co.jp>
2020-10-28 19:06:14 +0900

    :white_check_mark: Add get_recent_blog_title method spec

--- spec/hinatazaka_spec.rb
+++ spec/hinatazaka_spec.rb
@@ -1,6 +1,14 @@
 RSpec.describe(SakamichiScraper::Hinatazaka) do
-  it "Should get blog page title for hinatazaka-46" do
-    hinatazaka = SakamichiScraper::Hinatazaka.new
-    expect(hinatazaka.get_blog_top_page_title).to eq "日向坂46公式ブログ | 日向坂46公式サイト"
+  context "日向坂46の公式サイトにアクセスした際" do
+    before(:all) do
+      @hinatazaka = SakamichiScraper::Hinatazaka.new
+    end
+    it "日向坂46のブログページのタイトルが返却されること" do
+      expect(@hinatazaka.get_blog_top_page_title).to eq "日向坂46公式ブログ | 日向坂46公式サイト"
+    end
+
+    it "日向坂46の最新ブログ記事のタイトルが返却されること" do
+      expect(@hinatazaka.get_recent_blog_title).not_to eq nil
+    end
   end
 end
